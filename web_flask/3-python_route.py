ce39b876f7f2b123a8541d5afad50a40baaf6509
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-03 03:23:45 -0700

    Add Flask routes for '/' '/hbnb' '/c/<text>' and '/python/<text>'

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Route for the URL / """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Route for the URL /hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_txt(text):
+    """ Dynamic route for the URL /c/<text> """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_txt(text):
+    """ Routes for the URL /python/ and /python/<text> """
+    return "python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
