6e9a2b03290d9eac378f69c52ef7e8cd06f9846c
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-03 04:34:20 -0700

    change variable name

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,7 @@ def n_is_number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def n_template(n):
     """Route for the URL /number_template/<n> only displays if n is an int"""
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
