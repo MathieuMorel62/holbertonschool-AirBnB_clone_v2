248e7ccd6a5fdf0292f7053159a44e85805876ee
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-03 04:14:13 -0700

    Added a new route '/number_template/<n>' to display an HTML page with the number n if it's an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Route for the URL / """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Route for the URL /hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_txt(text):
+    """ Dynamic route for the URL /c/<text> """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_txt(text):
+    """ Routes for the URL /python/ and /python/<text> """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_number(n):
+    """ Route for the URL /number/<n>, only displays if n is an integer """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def n_template(n):
+    """Route for the URL /number_template/<n> only displays if n is an int"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
