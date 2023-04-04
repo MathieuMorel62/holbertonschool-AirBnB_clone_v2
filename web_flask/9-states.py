e459ea2b6f5fc6d3cb1c741dee61375dd94c2d74
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-04 08:12:41 -0700

    Combine states routes into single function

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,23 +11,22 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects"""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=states_sorted)
-
-
 @app.route("/states/<id>", strict_slashes=False)
-def states_cities(id):
-    """Displays an HTML page with a list of City objects linked to a State."""
-    states = storage.all(State)
-    state = states.get('State.{}'.format(id))
-    if state:
-        cities_sorted = sorted(state.cities, key=lambda city: city.name)
-        return render_template('9-states.html', state=state, cities=cities_sorted)
+def states_list():
+    """Displays an HTML page with a list of all State objects or cities"""
+    if id is None:
+        states = storage.all(State).values()
+        states_sorted = sorted(states, key=lambda state: state.name)
+        return render_template('9-states.html', states=states_sorted)
     else:
-        return render_template('9-states.html', not_found=True)
+        states = storage.all(State)
+        state = states.get('State.{}'.format(id))
+        if state:
+            cities_sorted = sorted(state.cities, key=lambda city: city.name)
+            return render_template(
+                '9-states.html', state=state, cities=cities_sorted)
+        else:
+            return render_template('9-states.html', not_found=True)
 
 
 @app.teardown_appcontext
