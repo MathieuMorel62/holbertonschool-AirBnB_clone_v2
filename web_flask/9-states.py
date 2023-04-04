8af2cc18c0bd0185e3c67aeae08edba7b75afbc5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-04-04 15:52:09 -0700

    Update states_list function and template for correct display

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,23 +11,19 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of states sorted by name"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
 @app.route("/states/<id>", strict_slashes=False)
-def states_list(id=None):
+def cities_list(id):
     """Displays an HTML page with a list of all State objects or cities"""
-    if id is None:
-        states = storage.all(State).values()
-        states_sorted = sorted(states, key=lambda state: state.name)
-        return render_template('9-states.html', states=states_sorted)
-    else:
-        states = storage.all(State)
-        state = states.get('State.{}'.format(id))
-        if state:
-            cities_sorted = sorted(state.cities, key=lambda city: city.name)
-            no_cities = len(cities_sorted) == 0
-            return render_template(
-                '9-states.html', state=state, cities=cities_sorted, no_cities=no_cities)
-        else:
-            return render_template('9-states.html', not_found=True, cities=None)
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
 
 
 @app.teardown_appcontext
@@ -37,4 +33,4 @@ def close_db(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
